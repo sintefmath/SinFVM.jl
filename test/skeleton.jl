7daefd317103a528076178f3c4683c96854e5d7d
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-01-15 14:32:37 +0100

    Added more timing benchmark to skeleton.jl

--- test/skeleton.jl
+++ test/skeleton.jl
@@ -1,5 +1,7 @@
 using Plots
+using Cthulhu
 using StaticArrays
+
 
 module Correct
 include("fasit.jl")
@@ -7,7 +9,7 @@ end
 using SinSWE
 function run_simulation()
     u0 = x -> sin.(2π * x)
-    nx = 16 * 1024
+    nx = 64*1024
     grid = SinSWE.CartesianGrid(nx)
 
     equation = SinSWE.Burgers()
@@ -25,17 +27,31 @@ function run_simulation()
     t = 0.0
 
     T = 1.0
-    plot(x, first.(SinSWE.current_interior_state(simulator)))
+    # plot(x, first.(SinSWE.current_interior_state(simulator)))
+    println("Running SinSWE twice")
     @time SinSWE.simulate_to_time(simulator, T)
-
+    SinSWE.set_current_state!(simulator, initial)
+    @time SinSWE.simulate_to_time(simulator, T)
     plot!(x, first.(SinSWE.current_interior_state(simulator)))
 
 
     number_of_x_cells = nx
     number_of_saves = 100
 
-    @time xcorrect, ucorrect, _, _ = Correct.solve_fvm(x -> sin(2π * x), T, number_of_x_cells, number_of_saves, Correct.Burgers())
-    plot!(xcorrect, ucorrect)
+    println("Running bare bones twice")
+    @time xcorrect, ucorrect, _, _ = Correct.solve_fvm(u0, T, number_of_x_cells, number_of_saves, Correct.Burgers())
+    @time xcorrect, ucorrect, _, _ = Correct.solve_fvm(u0, T, number_of_x_cells, number_of_saves, Correct.Burgers())
+    pref = plot!(xcorrect, ucorrect)
+    display(pref)
+    eigenvalue(u) = SinSWE.compute_max_abs_eigenvalue(simulator.system.equation, XDIR, u)
+    
+    @time SinSWE.compute_wavespeed(simulator.system, grid, initial)
+    @time SinSWE.compute_wavespeed(simulator.system, grid, initial)
+    @time SinSWE.compute_wavespeed(simulator.system, grid, initial)
+
+    @time maximum(eigenvalue.(initial))
+    @time maximum(eigenvalue.(initial))
+    @time maximum(eigenvalue.(initial))
 
 end
 
