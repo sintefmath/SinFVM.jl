c5b443c868be445d8729ee4b85b82eaa4c571627
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-03-07 21:09:15 +0100

    Fixed no-reconstruction to copy all array

--- src/reconstruction.jl
+++ src/reconstruction.jl
@@ -3,9 +3,6 @@ abstract type Reconstruction end
 struct NoReconstruction <: Reconstruction end
 
 function reconstruct!(backend, ::NoReconstruction, output_left, output_right, input_conserved, grid::Grid, equation::Equation, direction::XDIRT)
-    @fvmloop for_each_inner_cell(backend, grid, direction) do ileft, imiddle, iright
-        output_left[imiddle] = input_conserved[imiddle]
-        output_right[imiddle] = input_conserved[imiddle]
-        nothing
-    end
+    output_left .= input_conserved
+    output_right .= input_conserved
 end
