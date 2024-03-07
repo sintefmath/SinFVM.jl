2729e85671020b6333a9cd1370f38123fa0f0ac9
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-03-07 21:06:15 +0100

    Added SWE1D

--- src/equation.jl
+++ src/equation.jl
@@ -1,5 +1,35 @@
 abstract type Equation end
 
+struct ShallowWaterEquations1D{T} <: Equation
+    ρ::T
+    g::T
+end
+
+ShallowWaterEquations1D() = ShallowWaterEquations1D(1.0, 9.81)
+
+function (eq::ShallowWaterEquations1D)(::XDIRT, h, hu)
+    ρ = eq.ρ
+    g = eq.g
+    return @SVector [
+        ρ * hu,
+        ρ * hu * hu / h + 0.5 * ρ * g * h^2,
+    ]
+end
+
+function compute_eigenvalues(eq::ShallowWaterEquations1D, ::XDIRT, h, hu)
+    g = eq.g
+    u = hu / h
+    return @SVector [u + sqrt(g*h), u - sqrt(g*h)]
+end
+
+function compute_max_abs_eigenvalue(eq::ShallowWaterEquations1D, ::XDIRT, h, hu)
+    # TODO: Use compute_eigenvalues
+    g = eq.g
+    u = hu / h
+    return max(abs(u + sqrt(g*h)), abs(u - sqrt(g*h)))
+end
+number_of_conserved_variables(::ShallowWaterEquations1D) = 2
+
 struct ShallowWaterEquations{T} <: Equation
     ρ::T
     g::T
