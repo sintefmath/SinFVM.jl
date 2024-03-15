14d760ec57bf5b5bf8818983037b5eba4f50e3a3
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-03-15 09:50:19 +0100

    Testing for SVector in for_each_value_test

--- test/test_for_each_value.jl
+++ test/test_for_each_value.jl
@@ -1,5 +1,6 @@
 using Test
 using SinSWE
+using StaticArrays
 @show get_available_backends()
 
 for backend in get_available_backends()
@@ -8,10 +9,23 @@ for backend in get_available_backends()
     output = SinSWE.convert_to_backend(backend, zeros(10))
 
     SinSWE.@fvmloop SinSWE.for_each_index_value(backend, values_backend) do index, value
-        output[value] = value * 2
+        output[index] = value * 2
     end
 
     output = collect(output)
     @show output == 2 .* values
     @test output == 2 .* values
+
+
+    values_svector = [SVector{2,Float64}(i, 2 * i) for i in collect(1:10)]
+    values_svector_backend = SinSWE.convert_to_backend(backend, values_svector)
+    output_svector = SinSWE.convert_to_backend(backend, zeros(10))
+
+    SinSWE.@fvmloop SinSWE.for_each_index_value(backend, values_svector_backend) do index, value
+        output_svector[index] = value[2] * 2
+    end
+
+    output = collect(output_svector)
+    @show output == 4 .* values
+    @test output == 4 .* values
 end
