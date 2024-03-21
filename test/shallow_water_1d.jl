a77066a5c71d9e1e057d48f7b2d7ac471ffeb97f
Håvard Heitlo Holm <havard.heitlo.holm@sintef.no>
2024-03-21 15:25:38 +0100

    benchmarking pure vs practical swe

--- test/shallow_water_1d.jl
+++ test/shallow_water_1d.jl
@@ -10,21 +10,24 @@ using SinSWE
 function run_simulation()
 
     u0 = x -> @SVector[exp.(-(x - 0.5)^2 / 0.001) .+ 1.5, 0.0 .* x]
-    nx = 1024*1024
+    nx = 1024
     grid = SinSWE.CartesianGrid(nx; gc=2)
-    #backend = make_cuda_backend()
     backend = make_cuda_backend()
+    #backend = make_cpu_backend()
 
     equation = SinSWE.ShallowWaterEquations1D(backend, grid)
+    pure_equation = SinSWE.ShallowWaterEquations1DPure()
     reconstruction = SinSWE.NoReconstruction()
     linrec = SinSWE.LinearReconstruction(1.05)
     numericalflux = SinSWE.CentralUpwind(equation)
+    pure_numericalflux = SinSWE.CentralUpwind(pure_equation)
     conserved_system =
         SinSWE.ConservedSystem(backend, reconstruction, numericalflux, equation, grid)
     timestepper = SinSWE.ForwardEulerStepper()
     linrec_conserved_system = 
         SinSWE.ConservedSystem(backend, linrec, numericalflux, equation, grid)
-
+    pure_linrec_conserved_system = 
+        SinSWE.ConservedSystem(backend, linrec, pure_numericalflux, pure_equation, grid)
     x = SinSWE.cell_centers(grid)
     initial = u0.(x)
     T = 0.05
@@ -48,10 +51,13 @@ function run_simulation()
 
     simulator = SinSWE.Simulator(backend, conserved_system, timestepper, grid)
     linrec_simulator = SinSWE.Simulator(backend, linrec_conserved_system, timestepper, grid; cfl=0.2)
+    pure_linrec_simulator = SinSWE.Simulator(backend, pure_linrec_conserved_system, timestepper, grid; cfl=0.2)
 
     
     SinSWE.set_current_state!(linrec_simulator, initial)
     SinSWE.set_current_state!(simulator, initial)
+    SinSWE.set_current_state!(pure_linrec_simulator, initial)
+    
 
 
     initial_state = SinSWE.current_interior_state(simulator)
@@ -66,10 +72,12 @@ function run_simulation()
     result = collect(SinSWE.current_state(simulator))
     @time SinSWE.simulate_to_time(simulator, T) 
     @time SinSWE.simulate_to_time(linrec_simulator, T)
+    @time SinSWE.simulate_to_time(pure_linrec_simulator, T)
 
     
     result = SinSWE.current_interior_state(simulator)
     linrec_results = SinSWE.current_interior_state(linrec_simulator)
+    pure_results = SinSWE.current_interior_state(pure_linrec_simulator)
     
     lines!(
         ax,
@@ -85,7 +93,7 @@ function run_simulation()
         x,
         collect(result.hu),
         linestyle=:dashdot,
-        color=:green,
+        color=:red,
         linewidth=8,
         label=L"hu^{\Delta x}(x, t)",
     )
@@ -103,15 +111,36 @@ function run_simulation()
         x,
         collect(linrec_results.hu),
         linestyle=:dashdot,
-        color=:purple,
+        color=:orange,
         linewidth=4,
         label=L"hu_2^{\Delta x}(x, t)",
     )
-    axislegend(ax, position=:lb)
-    axislegend(ax2, position=:lb)
+    lines!(
+        ax,
+        x,
+        collect(pure_results.h),
+        linestyle=:dot,
+        color=:green,
+        linewidth=4,
+        label=L"h_{pure}^{\Delta x}(x, t)",
+    )
+    lines!(
+        ax2,
+        x,
+        collect(pure_results.hu),
+        linestyle=:dashdot,
+        color=:green,
+        linewidth=4,
+        label=L"hu_{pure}^{\Delta x}(x, t)",
+    )
+    axislegend(ax, position=:lt)
+    axislegend(ax2, position=:lt)
 
     
     display(f)
+
+    @show sum(abs.(collect(pure_results.h) - collect(linrec_results.h)))
+    @show sum(abs.(collect(pure_results.hu) - collect(linrec_results.hu)))
 
 
 
