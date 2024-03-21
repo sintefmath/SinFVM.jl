51493f0cb6ab83e2f6ce4c68b6b58941858c51e1
Håvard Heitlo Holm <havard.heitlo.holm@sintef.no>
2024-03-21 16:01:30 +0100

    todo on almost dry state

--- src/timestepper.jl
+++ src/timestepper.jl
@@ -34,12 +34,15 @@ end
 
 function do_substep!(output, ts::TimeStepper, system::System, eq::ShallowWaterEquations1D, current_state, dt, timestep_computer, substep_number)
     # TODO: Change type of eq to AllPracticalSWE? If so, change also the content of the if statement
+    # TODO: Need to support accumulation of rain... Move this functionality to postproc_substep function?
     dt = do_substep!(output, ts, system, current_state, dt, timestep_computer, substep_number)
     @fvmloop for_each_cell(system.backend, system.grid) do index
+        
         b_in_cell = 0.5*(eq.B[index] + eq.B[index + 1])
         if output[index][1] - b_in_cell < eq.depth_cutoff
             output[index] = typeof(output[index])(b_in_cell, 0.0)
         end
+    
     end
     return dt
 end
