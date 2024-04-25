3a650b22b4dbedcd000aa812b0333c001611a87d
havahol <havard.heitlo.holm@sintef.no>
2024-04-25 14:48:14 +0200

    Infiltration with basic tests

---
+++ src/sourceterms/source_term_infiltration.jl
@@ -0,0 +1,60 @@
+struct HortonInfiltration{S,T} <: SourceTermInfiltration
+    # Empiric infiltration model, where the infiltration rate f (m/s) is given by
+    # f(t) = fc + (f0 - fc)*exp(-k*t)
+    # The default parameters are supposed to correspond to a sandy soil type.
+    # 
+    # Source: 
+    # Eq (9), Table  1, and Sections 2.2.1 and 3.1 in:
+    # Fernandez-Pato, Caviedes-Voullieme, Garcia-Navarro (2016), 
+    # "Rainfall/runoff simulation with  2D full shallow water equations: Sensitivity 
+    #  analysis and calibration of infiltration parameters".
+    # Journal of Hydrology, 536, 496-513 
+    # https://doi.org/10.1016/j.jhydrol.2016.03.021.
+    f0::S
+    fc::S
+    k::S
+    factor::T
+    function HortonInfiltration(grid::CartesianGrid{2}, backend; factor=1.0, fc=3.272e-5, f0=1.977e-4, k=2.43e-3)
+        if ndims(factor) == 0
+            factor = [factor for x in cell_centers(grid; interior=false)]
+        else
+            validate_infiltration_factor(factor, grid)
+        end
+        factor = convert_to_backend(backend, factor)
+        return new{typeof(f0), typeof(factor)}(f0, fc, k, factor)
+    end
+    HortonInfiltration(f0, fc, k, factor; should_never_be_called) = new{typeof(f0), typeof(factor)}(f0, fc, k, factor)
+end
+
+function Adapt.adapt_structure(
+    to,
+    infiltration::HortonInfiltration
+) 
+    f0 = Adapt.adapt_structure(to, infiltration.f0)
+    fc = Adapt.adapt_structure(to, infiltration.fc)
+    k  = Adapt.adapt_structure(to, infiltration.k)
+    factor = Adapt.adapt_structure(to, infiltration.factor)  
+    HortonInfiltration(f0, fc, k, factor; should_never_be_called=nothing)
+end
+
+
+function validate_infiltration_factor(factor, grid::Grid)
+    if size(factor) != size(grid) 
+        throw(DomainError("Infiltration factor should be of size $(size(grid)) but got $(size(factor))"))
+    end
+end
+
+
+function compute_infiltration(f::HortonInfiltration, t, index::CartesianIndex)
+    return f.factor[index]*(f.fc + (f.f0 - f.fc)*exp(-f.k*t))
+end
+
+
+
+
+function evaluate_source_term!(infiltration::SourceTermInfiltration, output, current_state, cs::ConservedSystem, t)
+    output_h = output.h
+    @fvmloop for_each_cell(cs.backend, cs.grid) do index
+        output_h[index] -= compute_infiltration(infiltration, t, index)
+    end
+end
---
+++ test/test_infiltration.jl
@@ -0,0 +1,60 @@
+using StaticArrays
+using LinearAlgebra
+using Test
+import CUDA
+
+using SinSWE
+
+
+function plain_infiltration(backend, infiltration, grid, T; t0=0.0)
+    equation = SinSWE.ShallowWaterEquations()
+    reconstruction = SinSWE.LinearReconstruction()
+    numericalflux = SinSWE.CentralUpwind(equation)
+
+    conserved_system =
+        SinSWE.ConservedSystem(backend, reconstruction, numericalflux, equation, grid, infiltration)
+    timestepper = SinSWE.ForwardEulerStepper()
+    simulator = SinSWE.Simulator(backend, conserved_system, timestepper, grid; t0=t0)
+
+    u0 = x -> @SVector[1.0, 0.0, 0.0]
+    x = SinSWE.cell_centers(grid)
+    initial = u0.(x)
+    SinSWE.set_current_state!(simulator, initial)
+
+    # TODO: Make callback function that obtains the accumulated infiltration and runoff
+
+    @time SinSWE.simulate_to_time(simulator, t0 + T)
+
+    results = SinSWE.current_interior_state(simulator)
+    @test all(collect(results.hu) .== 0.0)
+    @test all(collect(results.hv) .== 0.0)
+    return collect(results.h)
+end
+
+
+grid = SinSWE.CartesianGrid(10, 10; gc=2, extent=[0 100; 0 100])
+backend = SinSWE.make_cpu_backend()
+test_inf = SinSWE.HortonInfiltration(grid, backend)
+@test all(test_inf.factor .== 1.0)
+@test size(test_inf.factor) ==  size(grid)
+@test SinSWE.compute_infiltration(test_inf, 0.0, CartesianIndex(1, 1)) == test_inf.f0
+@test SinSWE.compute_infiltration(test_inf, 1e6, CartesianIndex(1, 1)) == test_inf.fc
+
+factor_bad_size = [1.0 for x in SinSWE.cell_centers(grid)]
+@test_throws DomainError SinSWE.HortonInfiltration(SinSWE.CartesianGrid(2,2), backend; factor=factor_bad_size)
+
+cuda_backend = SinSWE.make_cuda_backend()
+infiltration_cuda = SinSWE.HortonInfiltration(grid, cuda_backend)
+
+h_cpu = plain_infiltration(backend, test_inf, grid, 1000; t0=1e6)
+@test maximum(h_cpu) .≈ ( 1.0 - test_inf.fc*1000) atol=1e-10
+@test minimum(h_cpu) .≈ ( 1.0 - test_inf.fc*1000) atol=1e-10
+
+h_cuda = plain_infiltration(cuda_backend, infiltration_cuda, grid, 1000; t0=1e6)
+@test maximum(h_cpu) .≈ ( 1.0 - infiltration_cuda.fc*1000) atol=1e-10
+@test minimum(h_cpu) .≈ ( 1.0 - infiltration_cuda.fc*1000) atol=1e-10
+
+
+# grid_case1 = SinSWE.CartesianGrid(2000, 10;  gc=2, boundary=SinSWE.WallBC(), extent=[0.0 4000.0; 0.0 20.0])
+
+
