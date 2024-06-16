235c75301b8d55cb7b04c3045764cd5274553083
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-06-16 22:26:23 +0200

    Added nice plot at the end to example

--- test/test_swe_ad.jl
+++ test/test_swe_ad.jl
@@ -8,21 +8,51 @@ using ForwardDiff
 import KernelAbstractions
 using SinSWE
 
-function run_swe_2d_ad_simulation()
-    ADType = ForwardDiff.Dual{ForwardDiff.Tag{:swe,Float64},Float64,2}
+function run_swe_2d_ad_simulation(height_of_wall)
+    # Here we say that we want to have one derivative (height_of_wall)
+    ADType = ForwardDiff.Dual{ForwardDiff.Tag{:swe,Float64},Float64,1}
+    Partials = ForwardDiff.Partials{1,Float64}
+
     backend = SinSWE.KernelAbstractionBackend(KernelAbstractions.get_backend(ones(3)); realtype=ADType)
 
     backend_name = SinSWE.name(backend)
     nx = 256
     ny = 32
     grid = SinSWE.CartesianGrid(nx, ny; gc=2)
+    dx = SinSWE.compute_dx(grid)
+    dy = SinSWE.compute_dy(grid)
+    width_of_wall = 4
+    length_of_wall = 40
 
-    equation = SinSWE.ShallowWaterEquationsPure()
+    # Important AD stuff happens here:
+    # First allocate the array:
+    bottom_topography_array = zeros(ADType, size(grid) .+ (1, 1))
+
+    # Then we set the general slope with no derivative wrt to h, x or y.
+    for I in eachindex(bottom_topography_array)
+        bottom_topography_array[I] = 1.0 .- dx * I[1]
+    end
+
+    xposition_of_wall = 0.2
+    yposition_of_wall = 0.2
+    @show xposition_of_wall / dx
+    iposition_of_wall = ceil(Int64, xposition_of_wall / dx)
+    jposition_of_wall = ceil(Int64, yposition_of_wall / dy)
+    # Now we place the wall:
+    for j in 1:width_of_wall
+        for i in 1:length_of_wall
+            bottom_topography_array[i+iposition_of_wall, j+jposition_of_wall] += ADType(height_of_wall, Partials((1.0,)))
+        end
+    end
+
+    bottom_topography = SinSWE.BottomTopography2D(bottom_topography_array, backend, grid)
+    bottom_source = SinSWE.SourceTermBottom()
+    equation = SinSWE.ShallowWaterEquations(bottom_topography)
     reconstruction = SinSWE.LinearReconstruction()
     numericalflux = SinSWE.CentralUpwind(equation)
 
     conserved_system =
-        SinSWE.ConservedSystem(backend, reconstruction, numericalflux, equation, grid)
+        SinSWE.ConservedSystem(backend, reconstruction, numericalflux, equation, grid, [bottom_source])
     timestepper = SinSWE.ForwardEulerStepper()
     simulator = SinSWE.Simulator(backend, conserved_system, timestepper, grid)
     T = 0.05
@@ -40,6 +70,7 @@ function run_swe_2d_ad_simulation()
     axes = [[Axis(f[i, 2*j-1], ylabel=L"y", xlabel=L"x", title="$(titles[j])\n$(names[i])") for i in 1:3] for j in 1:2]
 
 
+    # IMPORTANT: To get the value, we need to do ForwardDiff.value
     current_simulator_state = ForwardDiff.value.(collect(SinSWE.current_state(simulator)))
     @test !any(isnan.(current_simulator_state))
 
@@ -74,6 +105,15 @@ function run_swe_2d_ad_simulation()
         Colorbar(f[3, 4], hm)
     end
     display(f)
+
+    # Now we can get the derivative of the height of the water wrt to the height of the building as
+
+    derivative = map(x -> x.partials[1], collect(result.h))
+    f = Figure(size=(1600, 1200), fontsize=24)
+    ax = Axis(f[1, 1])
+    hm = heatmap!(ax, derivative)
+    Colorbar(f[1, 2], hm)
+    display(f)
 end
 
-run_swe_2d_ad_simulation()
+run_swe_2d_ad_simulation(2.0)
