be6afab5393a5a8bff6d8734afad424a7e613fc8
SpeedyPeder <pederaasvarheim@gmail.com>
2026-02-24 10:58:22 +0100

    Added in a source term for the Coriolis force to experiment with. Important note! This will break the well-balanced properties of the schemes, but applies more relevant physics to ocean simulations.

---
+++ src/sourceterms/source_term_coriolis.jl
@@ -0,0 +1,43 @@
+# Copyright (c) 2024 SINTEF AS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+struct SourceTermCoriolis{T} <: SourceTerm
+    f::T   # Coriolis parameter 
+end
+
+# Single-layer 2D
+function evaluate_source_term!(st::SourceTermCoriolis, output, current_state, cs::ConservedSystem, _)
+    f = st.f
+    @fvmloop for_each_inner_cell(cs.backend, cs.grid) do imiddle
+        hu = current_state.hu[imiddle]
+        hv = current_state.hv[imiddle]
+        output.hu[imiddle] +=  f * hv
+        output.hv[imiddle] += -f * hu
+        nothing
+    end
+end
+
+# Two-layer 2D: applied independently to each layer
+function evaluate_source_term!(st::SourceTermCoriolis, output, current_state,
+                               cs::ConservedSystem{<:Any,<:Any,<:Any,<:TwoLayerShallowWaterEquations2D}, _)
+    f = st.f
+    @fvmloop for_each_inner_cell(cs.backend, cs.grid) do imiddle
+        output.q1[imiddle] +=  f * current_state.p1[imiddle]
+        output.p1[imiddle] += -f * current_state.q1[imiddle]
+        output.q2[imiddle] +=  f * current_state.p2[imiddle]
+        output.p2[imiddle] += -f * current_state.q2[imiddle]
+        nothing
+    end
+end
